45d386e29183137a57c1fc16383c81d4465d7d67
Victor Erike <Verike7@gmail.com>
2022-10-18 01:45:43 +0100

    Aligned with betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * 
+ * puts() - Displays a string to stdio
+ *
+ * Return - Always 0 (success)
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
