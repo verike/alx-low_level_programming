70a4390c308a34790bf33ccda1af5fbe6b4459a3
Victor Erike <Verike7@gmail.com>
2022-10-18 01:50:31 +0100

    Updated script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,10 @@
 /**
  * main - Entry point
  * 
- * puts() - Displays a string to stdio
- *
  * Return - Always 0 (success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
