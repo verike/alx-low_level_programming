8ea913e6c16e5884b800ec1130190ec3892ff94c
Victor Erike <Verike7@gmail.com>
2022-10-19 12:36:51 +0100

    Added comments

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+/**
+ * print_alphabet_x10() - prints alphabets in lowercase 10 times
+ */
 void print_alphabet_x10(void)
 {
 	int count = 0;
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <stdio.h>
 
+/*Prototype for print_alphabet function in 1-main.c file*/
 void print_alphabet(void);
 
+/*Prototype for print_alphabet_x10 function in 2-main.c file*/
 void print_alphabet_x10(void);
