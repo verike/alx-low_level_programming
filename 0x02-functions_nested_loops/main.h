33efaecd5f5bcb5e562be1fa91ac436ec0f1b811
Victor Erike <Verike7@gmail.com>
2022-10-19 18:03:04 +0100

    Updated: Added the prototype for _islower function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,12 @@
 #pragma once
 #include <stdio.h>
+#include <ctype.h>
 
-/*Prototype for print_alphabet function in 1-main.c file*/
-void print_alphabet(void);
+/*Prototype for _islower function in 3-main.c file*/
+int _islower(int c);
 
 /*Prototype for print_alphabet_x10 function in 2-main.c file*/
 void print_alphabet_x10(void);
+
+/*Prototype for print_alphabet function in 1-main.c file*/
+void print_alphabet(void);
