a3511ea7faa8a327ce70fa805f595d7a6eae087e
Victor Erike <Verike7@gmail.com>
2022-10-20 06:04:11 +0100

    Initial commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/*
+ * print_sign - Checks for polarity
+ *
+ * @n: only argument passed in
+ *
+ * Return: 1 || 0 || -1 || 404 for test feedback
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1) && _putchar('+');
+
+	}
+	else if (n == 0)
+	{
+		return (0) && _putchar(0);
+
+	}
+	else if (n < 0)
+	{
+		return (-1) && _putchar('-');
+
+	}
+	else
+	{
+		return (404) && _putchar('E');
+
+	}
+}
