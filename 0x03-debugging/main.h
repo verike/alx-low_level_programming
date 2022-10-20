f359064dea99efd4e0b3b1b16c30b6a8406414a7
Victor Erike <Verike7@gmail.com>
2022-10-20 21:30:34 +0100

    Updated: Included the prototype for 2-largest_number function

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
+
+/* Prototype for the 2-largest_number function in 2-largest_number.c file */
+int largest_number(int a, int b, int c);
 
 /* Prototype for the positive_or_negative function in positive_or_negative.c file */
 void positive_or_negative(int i);
