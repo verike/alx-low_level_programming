7b8143cf2433752a7c6be6e29690db2129ed59c5
Victor Erike <Verike7@gmail.com>
2022-10-20 22:46:25 +0100

    Updated: Removed whitespaces

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,10 +2,14 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
+ *
  * @a: first integer
+ *
  * @b: second integer
+ *
  * @c: third integer
- * Return: largest number
+ *
+ * Return: largest
  */
 
 int largest_number(int a, int b, int c)
