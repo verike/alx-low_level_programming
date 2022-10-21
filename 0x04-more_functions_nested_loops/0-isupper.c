25e799d1b26f071d4cf1f2e0ed5cceb4d21f7c33
Victor Erike <Verike7@gmail.com>
2022-10-21 23:06:23 +0100

    Initial commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - returns 1 if value is upper case and 0 if else
+ *
+ * @c: charater passed in as an argument
+ */
+int _isupper(int c)
+{
+	r = isupper(c);
+
+	if (r != 0)
+	{
+		return (1);
+
+	}
+	else
+	{
+		return (0):
+
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <ctype.h>
+
+/* Prototype for isupper function it isupper.c task */
+int _isupper(int c);
+
+#endif /* MAIN_H */
