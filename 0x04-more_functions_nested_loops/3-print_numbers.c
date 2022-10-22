63df76cc19cf70d9b3556cef640eb4b87391dfe1
Victor Erike <Verike7@gmail.com>
2022-10-22 07:04:35 +0100

    Initial commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints out numbers from 0 - 9
+ *
+ * Return: result
+ */
+void print_numbers(void)
+{
+	int result = 0;
+
+	while (result < 10)
+	{
+		printf("%d",result);
+
+		result++;
+	
+	}
+
+}
