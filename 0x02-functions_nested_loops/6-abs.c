778a410df2cd90aa50019db39952df9130307711
Victor Erike <Verike7@gmail.com>
2022-10-24 00:49:44 +0100

    Initial commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute value of the argument passed into it
+ *
+ * @n: argument passed
+ *
+ * Return: 'a' which is the absolute value
+ */
+int _abs(int n)
+{
+	int a;
+
+	a = abs(n);
+
+	return (a);
+
+}
+
---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - Entry point to this program
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int r;
+
+	r = _abs(-1);
+
+	printf("%d\n", r);
+
+	r = _abs(0);
+
+	printf("%d\n", r);
+
+	r = _abs(1);
+
+	printf("%d\n", r);
+
+	r = _abs(-98);
+
+	printf("%d\n", r);
+
+	return (0);
+
+}
