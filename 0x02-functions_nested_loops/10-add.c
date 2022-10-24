297bb4bb38ebfa071cc4cc91dcaa9c78883ffef6
Victor Erike <Verike7@gmail.com>
2022-10-24 02:10:58 +0100

    Initial commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - this adds two integers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: Always result
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+
+}
