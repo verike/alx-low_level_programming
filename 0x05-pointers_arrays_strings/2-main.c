54e65dcf5ee132300e043189f205eeb61dcbf816
Victor Erike <Verike7@gmail.com>
2022-10-25 23:45:53 +0100

    Initial commit

---
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *str;
+
+	int len;
+
+	str = "My first strlen!";
+
+	len = _strlen(str);
+
+	printf("%d\n",len);
+
+	return (0);
+
+}
