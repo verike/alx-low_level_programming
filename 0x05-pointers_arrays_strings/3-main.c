c18e80cbf6b01704810ec00576b50a9196e169ec
Victor Erike <Verike7@gmail.com>
2022-10-26 00:55:39 +0100

    Updated: Included prototype for 3-puts

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+
+	_puts(str);
+
+	return (0);
+
+}
---
+++ 0x05-pointers_arrays_strings/3-put.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts - Prints our any string passed into it
+ *
+ * @str: Char pointer argument passed into the function
+ */
+void _puts(char *str)
+{
+	_putchar(str);
+
+}
