ca21069d657b4e355344df09045748eadc2d0f72
Victor Erike <Verike7@gmail.com>
2022-10-25 23:45:07 +0100

    Updated: Included the string header file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - checks the length of a string
+ *
+ * @s: Char argument passed
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s);
+
+	return (len);
+
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* Prototype for _strlen function in 2-strlen.c file */
 int _strlen(char *s);
