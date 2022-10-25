d1fd69a73de4562c1d5290764e8d6308c5ec38ca
Victor Erike <Verike7@gmail.com>
2022-10-25 23:36:25 +0100

    Updated: Included prototype for _strlen

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Prototype for _strlen function in 2-strlen.c file */
+int _strlen(char *s);
+
 /* Prototype for swap_int function in 1-swap.c file */
 void swap_int(int *a, int *b);
 
