e9b8e6d6a367ab88ac4ff577ea386046dde8fbce
Victor Erike <Verike7@gmail.com>
2022-10-26 03:03:34 +0100

    Initial commit

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of then - Isaac Asimov";
+
+	print_rev(str);
+
+	return (0);
+
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - Revers the output display
+ *
+ * @s: String argument passed in
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+		for (i = i - 1; i >= 0; i--)
+		{
+			_putchar(s[i]);
+
+		}
+		_putchar('\n');
+
+}
