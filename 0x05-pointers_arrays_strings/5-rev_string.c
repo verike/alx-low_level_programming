6e62c198387986c1782b5d8cb6b054b8b67ef400
Victor Erike <Verike7@gmail.com>
2022-10-28 02:03:32 +0100

    Updated the source code

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,19 +4,22 @@
  * rev_string - Reverses any string passed in
  *
  * @s: String argument passed into the function
+ *
+ * Return: returns reverse
  */
 void rev_string(char *s)
 {
+	char rv = s[0];
+	int c = 0;
 	int i;
 
-	char *str[] = s;
-
-	while (str[i] != '\0')
-		i++;
-
-	for (i = i - 1; i >= 0; i--)
+	while (s[c] != '\0')
+		c++;
+	for (i = 0; i < c; i++)
 	{
-		_putchar(str[i]);
-
+		c--;
+		rv = s[i];
+		s[i] = s[c];
+		s[c] = rv;
 	}
 }
