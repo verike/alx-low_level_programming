46a7ffdf74c7c8412912204d68cb3276137dc5df
Victor Erike <Verike7@gmail.com>
2022-11-05 23:41:12 +0100

    Initial commit

---
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_line(0);
+
+	print_line(2);
+
+	print_line(10);
+
+	print_line(-4);
+
+	return (0);
+
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_line - Draws line with a given dimension
+ *
+ * @n: dimension passed in as an argument
+ */
+void print_line(int n)
+{
+	int i = 1;
+
+	if (i <= n)
+	{
+		while (i <= n)
+		{
+			_putchar('_');
+			
+			i++;
+
+		}
+
+		_putchar('\n');
+
+	}
+	else if (n <= 0)
+	{
+		_putchar('\n');
+
+	}
+}
