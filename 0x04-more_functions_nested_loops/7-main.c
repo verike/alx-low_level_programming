4830727f66fa956bf836eb10aef63ab7be0876f4
Victor Erike <Verike7@gmail.com>
2022-11-07 07:51:37 +0100

    Initialcommit

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_diagonal(0);
+
+	print_diagonal(2);
+
+	print_diagonal(10);
+
+	print_diagonal(-4);
+
+	return (0);
+
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			if (i > 1)
+			{
+				j = i;
+
+				while ((j - 1) > 0)
+				{
+
+					_putchar(' ');
+				       
+					j--;
+
+				}
+			}
+			_putchar('\\');
+
+			_putchar('\n');
+
+		}
+
+	}
+	else
+	{
+		_putchar('\n');
+
+	}
+}
