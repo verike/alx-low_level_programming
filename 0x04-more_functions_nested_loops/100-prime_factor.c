ae968ed49f750004d0bb97062d8976131d7dbb05
Victor Erike <Verike7@gmail.com>
2022-11-08 21:09:38 +0100

    Updated: Added a new variable (j) for contrast purpose

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,21 +7,23 @@
  */
 int main(void)
 {
-	unsigned long num = 612852475143, i;
+	long int num = 612852475143, i, j;
 
-	for (i = 2; i < num; i++)
+	for (i = 2; i <= num; i++)
 	{
+
 		while (num % i == 0)
 		{
-			num = num / i;
+			j = i;
+
+			num = num / j;
 
 			if (num == 1)
 			{
-				printf("%lu \n", i);
-
-				break;
+				printf("%lu \n", j);
 
 			}
+			j++;
 		}
 	}
 	return (0);
