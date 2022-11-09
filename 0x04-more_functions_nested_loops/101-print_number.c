9953283863c45bfc0b1366778db2bc9f108cf825
Victor Erike <Verike7@gmail.com>
2022-11-09 20:00:59 +0100

    Initial commit

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer to standard output
+ *
+ * @n: Argument passed
+ */
+void print_number(int n)
+{
+	n++;
+
+	_putchar(n - 1 + '0');
+
+}
