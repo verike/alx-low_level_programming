24134e0665c80dea937c0e276ecdf72e5869e7cd
Victor Erike <Verike7@gmail.com>
2022-11-10 15:54:17 +0100

    Initial commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string of char into a pointer
+ *
+ * @dest: pointer for storing new string copied
+ *
+ * @src: source where the string of char will be copied from
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	
+	dest = strcpy(dest , src);
+
+	return (dest);
+
+}
