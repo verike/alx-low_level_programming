642f030c17c8de601aae5fc6e7162845755ea184
Victor Erike <Verike7@gmail.com>
2022-11-11 22:11:07 +0100

    Updated: Removed trailing spaces

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _atoi - filters a string with integers and returns only the integer
+ *
+ * @s: String passed in as an argument
+ *
+ * Return: Returns the interger stored in the variable 'c'
+ */
 int _atoi(char *s)
 {
 	int c;
--- 0x05-pointers_arrays_strings/100-main.c
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -1,39 +1,36 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	int nb;
 
 	nb = _atoi("89");
-
 	printf("%d \n", nb);
 
 	nb = _atoi("-402");
-
 	printf("%d \n", nb);
 
 	nb = _atoi("          ------++++++-----+++++--98");
-
 	printf("%d \n", nb);
 
 	nb = _atoi("214748364");
-
 	printf("%d \n", nb);
 
 	nb = _atoi("0");
-
 	printf("%d \n", nb);
 
 	nb = _atoi("Suite 402");
-
 	printf("%d \n", nb);
 
-	nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-
+	nb = _atoi("++ - -98 Battery Street; San Francisco, CA 94111 - USA ");
 	printf("%d \n", nb);
 
 	nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-
 	printf("%d \n", nb);
 
 	return (0);
